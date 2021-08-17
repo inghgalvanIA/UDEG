c35d7f521b6f13c16bf09c2a833f7625c88ac88c
Hector Galvan <inghgalvan@outlook.com>
2021-08-16 21:40:02 -0500

    actividad 2 programacion 1

--- programacion_uno/area_cono/area_cono_aut.js
+++ programacion_uno/area_cono/area_cono_aut.js
@@ -1,34 +1,26 @@
 /* 
     Hector Rosendo Galvan Rivas
-    id estudiante:221968633 
     Fundamentos de programaciòn
     Actividad 2 Funciones 
-    referencias:
-    https://www.youtube.com/watch?v=iWnLNGNh49o&t=108s
-    https://www.youtube.com/watch?v=Cal94N065cA
 
 */
+// var altura = 4;
+// var radio = 3;
+// var generatriz;
 
-//ingresamos daots por medio de variables
- var altura = 4;
- var radio = 3;
-
-//funcion
-function cono (altura,radio){
-
+function cono (altura,radio, generatriz){
+    var altura = parseInt(document.getElementById("altura").value);
+    var radio = parseInt(document.getElementById("radio").value);
     //obtener la generatriz
-    //raiz cuadrada(altura**2 + radio**2)
     suma = altura**2 + radio**2
     generatriz = Math.sqrt(suma)
     console.log("la generatriz es:");
     console.log(generatriz + " cm");
     //obtener el area lateral
-    //Alateral = pi * (g*r)
     area_lateral = 3.1416 * (generatriz * radio)
     console.log("el area lateral del cono es:")
     console.log(area_lateral +  " cm**2")
     //obtener el ara de la base
-    //Abase = pi * radio**2
     area_base = 3.1416 * (radio**2)
     console.log("el area base del cono es:")
     console.log(area_base +  " cm**2")
@@ -36,14 +28,18 @@ function cono (altura,radio){
     //At = Alateral + Abase
     area_total = area_lateral + area_base
     console.log("el area total del cono es:")
-    console.log(area_total.toFixed(2) +  " cm**2")
-    //obtener el volumne
-    //v = (Abase * Altura)/3
+    console.log(area_total +  " cm**2")
+    //volumen
     volumen = (area_base * altura)/3
     console.log("el volumen del cono es:")
     console.log(volumen.toFixed(2) +  " cm**2")
+    //alert(area_total + "cm**2")
+    document.getElementById("el_resultado_area").innerHTML = area_total.toFixed(2) + "cm**2";
+    document.getElementById("el_resultado_volumen").innerHTML = volumen.toFixed(2) + "cm**2";
 }
 
+function limpiar(){
+    document.getElementsByTagName("miformulario").reset();
+}
 
-//mandar llamar a la funcion
-cono(altura,radio);
+cono(altura,radio,generatriz);
